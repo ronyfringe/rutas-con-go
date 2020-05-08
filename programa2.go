15116d897aec0b7a9187b4ddc0324e5d947eceeb
zaza <contact@zaza.com>
2020-05-07 20:59:23 -0500

    primer commit

---
+++ programa2.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+
+	//routes
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+
+	// start the server
+	http.ListenAndServe(":3000", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Contact Page"))
+}
